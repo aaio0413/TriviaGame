ce3cf7d386208c99828ba3fc54bc603f31d69c14
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-06-22 18:43:27 -0700

    Modifyied question generator. Shuffle method for array. Added some buttons. Still need to fix some issues.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,22 +3,48 @@ questions[0] = {
     question: 'Tokyo was called \'EDO\' before. How many years ago did the city became TOKYO?',
     answers: ['400 years ago.', '800 years ago.', '200 years ago.', '100 years ago.']
 }
+questions[1] = {
+    question: 'Q2',
+    answers: ['400 years ago.', '800 years ago.', '200 years ago.', '100 years ago.']
+}
+questions[2] = {
+    question: 'Q3',
+    answers: ['400 years ago.', '800 years ago.', '200 years ago.', '100 years ago.']
+}
+questions[3] = {
+    question: 'Q4',
+    answers: ['400 years ago.', '800 years ago.', '200 years ago.', '100 years ago.']
+}
+questions[4] = {
+    question: 'Q5',
+    answers: ['400 years ago.', '800 years ago.', '200 years ago.', '100 years ago.']
+}
+
 
 var clockRunning = false;
 var time = 30;
 var interval;
-var questionNum = 0;
+var questionCount = 0;
+
 
 $(document).ready(function() {
     $('#start-button').on('click', function() {
         $('.hm-body').css('background-image', 'url(assets/images/FUGOKU.jpg)');
         $('.start-button-wrapper').hide();
         $('#game').toggle();
+        shuffleArray(questions);
         start();
 
     })
 });
 
+var shuffleArray = function(array) {
+    for (let i = array.length -1; i >0; i--) {
+        const j = Math.floor(Math.random()* (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
 
 
@@ -33,10 +59,21 @@ var start = function() {
 
 var timeCount = function() {
     time--;
-    var convertedTime = timeConverter(time);
-    $('#time-display').text(convertedTime);
+    if (time < 0) {
+        stop();
+        jumpToNext();
+    } else {
+        var convertedTime = timeConverter(time);
+        $('#time-display').text(convertedTime);
+    }
+
     
 };
+
+var stop = function() {
+    clearInterval(interval);
+    clockRunning = false;
+}
 
 var timeConverter = function(t) {
     var minutes = Math.floor(t / 60);
@@ -52,52 +89,61 @@ var timeConverter = function(t) {
 };
 
 var displayQuestion = function() {
-    // var qDiv = $('<div>'+ question[0].q+ '</div>');
-    // qDiv.addClass('question')
-    $('#question').html(questions[0].question);
-    // for(i=0; i < questions[0].answers.length; i++) {
-    //     var answer = 
-    // }
-    for (i=0; i<questions[questionNum].answers.length; i++) {
-        var answer1 = $('<input class=\'answer\' name=\'answers\' type=\'radio\' value>');
-        answer1.text(questions[questionNum].answers[i]);
-        answer1.attr('value', questions[questionNum].answers[i]);
-        
-        var answer1P = $('<span>'+ questions[questionNum].answers[i] + '</span>')
-        $('#answer'+i).append(answer1);
-        $('#answer'+i).append(answer1P);
+    if(questionCount === questions.length-1) {
+        console.log('END THE GAME')
+    } else {
+        $('#question').html(questions[questionCount].question);
+
+        for (i=0; i<questions[questionCount].answers.length; i++) {
+            var answer1 = $('<input class=\'answer\' name=\'answers\' type=\'radio\' value>');
+            answer1.text(questions[questionCount].answers[i]);
+            answer1.attr('value', questions[questionCount].answers[i]);
+            
+            var answer1P = $('<span>'+ questions[questionCount].answers[i] + '</span>')
+            $('#answer'+i).append(answer1);
+            $('#answer'+i).append(answer1P);
+        }
+        questionCount++;
     }
-
-    // var answer1 = $('<input class=\'answer\' name=\'answers\' type=\'radio\' value>');
-    // answer1.text(questions[0].answers[0]);
-    // answer1.attr('value', questions[0].answers[0]);
     
-    // var answer1P = $('<span>'+ questions[0].answers[0] + '</span>')
-    // $('#answer1').append(answer1);
-    // $('#answer1').append(answer1P);
-
-
-
-    // $('#answer1').html(questions[0].answers[0]);
-    // $('#answer2').html(questions[0].answers[1]);
-    // $('#answer3').html(questions[0].answers[2]);
-    // $('#answer4').html(questions[0].answers[3]);
-
-    // var questionDiv
 };
 
-$('#submit-button').on('click', function() {
+
+var jumpToNext = function() {
+    $('#board').toggle();
+    $('#the-answer-wrapper').toggle();
     let selectedAnswer = $('input[name="answers"]:checked').val();
     console.log(selectedAnswer);
-})
+    $('#the-answer').text(questions[questionCount].answers[0]);
+    if(selectedAnswer === questions[questionCount].answers[0]) {
+        console.log('YOUR ANSWER IS CORRECT')
+    } else {
+        console.log('YOUR ANSWER IS NOT CORRECT')
+    }
 
-var jumpToNextPage = function() {
-    
 }
 
 $('#submit-button').on('click', function() {
+    jumpToNext();
+    stop();
+});
+// $('#submit-button').on('click', function() {
+//     $('#board').toggle();
+//     $('#the-answer-wrapper').toggle();
+//     $('#question').text(questions[questionCount].answers[0]);
+// })
+
+$('#next-button').on('click', function() {
     $('#board').toggle();
     $('#the-answer-wrapper').toggle();
-    $('#question').html(questions[questionNum].answers[0]);
+    displayQuestion();
+    cleanQuestions();
+    time = 30;
+    start();
 })
 
+var cleanQuestions = function() {
+    var originalQuestionsTgs = '<p id=\'answer0\'>1.</p><p id=\'answer1\'>2.</p><p id=\'answer2\'>3.</p><p id=\'answer3\'>4.</p>'
+    $('#answers-wrapper').replaceWith(originalQuestionsTgs);
+
+}
